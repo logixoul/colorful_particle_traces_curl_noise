9fb65132511c5fe3da46c9d1306b177378856845
Stefan Monov <stefan.monov@gmail.com>
2017-05-28 00:14:28 +0300

    .

--- src/main4.cpp
+++ src/main4.cpp
@@ -15,7 +15,7 @@
 #include "colorspaces.h"
 #include "easyfft.h"
 
-int wsx=800, wsy = 800 * (800.0f / 1280.0f);
+int wsx=1000, wsy = 1000 * (800.0f / 1280.0f);
 int scale = 1;
 int sx = wsx / scale;
 int sy = wsy / scale;
@@ -39,6 +39,73 @@ Vec3f complexToColor_HSV(Vec2f comp) {
 	//lightness /= lightness + 1.0f;
 	HslF hsl(hue, 1.0f, lightness);
  	return FromHSL(hsl);
+}
+
+// https://www.shadertoy.com/view/4dK3zG
+Vec3f complexToColor_fromShaderToy(Vec2f comp) {
+	float hue = (float)M_PI+(float)atan2(comp.y,comp.x);
+	hue /= (float)(2*M_PI);
+	float normal_value = hue;
+    Vec3f color;
+    if(normal_value<0.0) normal_value = 0.0;
+    if(normal_value>1.0) normal_value = 1.0;
+    float v1 = 1.0/7.0;
+    float v2 = 2.0/7.0;
+    float v3 = 3.0/7.0;
+    float v4 = 4.0/7.0;
+    float v5 = 5.0/7.0;
+    float v6 = 6.0/7.0;
+    //compute color
+    if(normal_value<v1)
+    {
+      float c = normal_value/v1;
+      color.x = 70.*(1.-c);
+      color.y = 70.*(1.-c);
+      color.z = 219.*(1.-c) + 91.*c;
+    }
+    else if(normal_value<v2)
+    {
+      float c = (normal_value-v1)/(v2-v1);
+      color.x = 0.;
+      color.y = 255.*c;
+      color.z = 91.*(1.-c) + 255.*c;
+    }
+    else if(normal_value<v3)
+    {
+      float c = (normal_value-v2)/(v3-v2);
+      color.x =  0.*c;
+      color.y = 255.*(1.-c) + 128.*c;
+      color.z = 255.*(1.-c) + 0.*c;
+    }
+    else if(normal_value<v4)
+    {
+      float c = (normal_value-v3)/(v4-v3);
+      color.x = 255.*c;
+      color.y = 128.*(1.-c) + 255.*c;
+      color.z = 0.;
+    }
+    else if(normal_value<v5)
+    {
+      float c = (normal_value-v4)/(v5-v4);
+      color.x = 255.*(1.-c) + 255.*c;
+      color.y = 255.*(1.-c) + 96.*c;
+      color.z = 0.;
+    }
+    else if(normal_value<v6)
+    {
+      float c = (normal_value-v5)/(v6-v5);
+      color.x = 255.*(1.-c) + 107.*c;
+      color.y = 96.*(1.-c);
+      color.z = 0.;
+    }
+    else
+    {
+      float c = (normal_value-v6)/(1.-v6);
+      color.x = 107.*(1.-c) + 223.*c;
+      color.y = 77.*c;
+      color.z = 77.*c;
+    }
+    return color * 1.5f / 255.0f;
 }
 
 Vec3f complexToColor(Vec2f comp) {
